c1b936d3140d16107deaf6fbd9ab3863db2bff3c
Ayelen may <Ayelen.kirchhoff@gmail.com>
2025-07-04 18:18:20 +0200

    step 2, add filter_users_by_name funciton

--- filter_users.py
+++ filter_users.py
@@ -1,33 +1,21 @@
-def filter_by_age(users, min_age):
-
-    """Return a list of users whose age is greater or equal to min_age"""
-
-    return [user for user in users if user.get("age", 0) >= min_age]
+import json
 
 
-def filter_by_email(users, email_substring):
+def filter_users_by_name(name):
+    with open("users.json", "r") as file:
+        users = json.load(file)
 
-    """Returns users whose email contains the given substring
-    -users(list of dict) -email_substring (str)
-    returns- -list """
+    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
-    return [user for user in users if email_substring.lower() in user.get("email", "").lower()]
-
-if __name__=="__main__":
+    for user in filtered_users:
+        print(user)
 
 
-    users = [
-        {"name": "Alice", "email": "alice@gmail.com", "age": 24},
-        {"name": "Bob", "email": "bob@gmail.com", "age": 19},
-        {"name": "dylan", "email": "dylan@yahoo.com", "age":49},
-    ]
+if __name__ == "__main__":
+    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
 
-"""filter by age"""
-print("Users age 20 and above: ")
-result_age = filter_by_age(users, 20)
-print(result_age)
-
-"""filter by email"""
-print("Users with email containing 'gmail': ")
-result_email = filter_by_email(users,"gmail")
-print(result_email)
+    if filter_option == "name":
+        name_to_search = input("Enter a name to filter users: ").strip()
+        filter_users_by_name(name_to_search)
+    else:
+        print("Filtering by that option is not yet supported.")
