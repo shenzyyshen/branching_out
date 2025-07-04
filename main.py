8a28172966750265b6c653336826332b8ae943a3
Ayelen may <Ayelen.kirchhoff@gmail.com>
2025-07-04 20:28:02 +0200

    User input implmentation, main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-def filter_by_age(users, min_age):
-    return [user for user in users if user.get("age", 0) >= min_age]
+from filter_users import filter_users_by_name, filter_users_by_age
+
+
+
+if __name__=="__main__":
+    filter_option = input("What would you like to filter by? (age or name) ").strip().lower()
+
+    if filter_option == "name":
+        name_to_search = input("Enter a name to filter users: ").strip()
+        filter_users_by_name(name_to_search)
+
+    elif filter_option == "age":
+        try:
+            min_age = int(input("Enter minimum age to filter users: ").strip())
+            filter_users_by_age(min_age)
+
+        except ValueError:
+            print("please enter a valid number.")
+    else:
+        print("filtering by that option is not yet supported. ")
